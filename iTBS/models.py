a0d9eb5b08b047231455815a19def00fedb2bab3
trizzle22 <137146278+trizzle22@users.noreply.github.com>
2024-08-13 11:32:38 +0200

    Update models.py

--- iTBS/models.py
+++ iTBS/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+first_name = models.CharField(max_length=50)
+last_name = models.CharField(max_length=50)
+student_number = models.CharField(max_length=15, unique=True)
+def __str__(self):
+return f"{self.first_name} {self.last_name}"
